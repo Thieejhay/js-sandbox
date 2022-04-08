387d32959ec2be6641122ab9f12bb07c90413ca0
Thieejhay <101231923+Thieejhay@users.noreply.github.com>
2022-04-08 19:46:13 +0100

    Create object.js

---
+++ object.js
@@ -0,0 +1,27 @@
+var user = {
+	username: "Tijani",
+	password: "pass"
+};
+
+var database = [
+	{
+		username: "Tijani",
+		password: "pass"
+	}
+];
+
+var newsfeed = [
+	{
+		username: "",
+		timeline: ""
+	},
+	{
+		username: "",
+		timeline: ""
+	},
+	{
+		username: "",
+		timeline: ""
+	}
+
+];
